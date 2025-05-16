fbdf8bed30661a8c78177367ab6b5799f6496062
ishan09 <ishansharma191@gmail.com>
2025-05-17 01:57:03 +0530

    commented tests

--- src/test/java/com/example/productcatalogueservice/ProductCatalogueServiceApplicationTests.java
+++ src/test/java/com/example/productcatalogueservice/ProductCatalogueServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductCatalogueServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
